e51bcffee683a3d78b75d528c49f19e9e35933c3
Hernandes-Silva <lertanisslobr@gmail.com>
2021-03-26 15:14:35 -0300

    colocando os gates, aplicação aparentimente terminada

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,30 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreAdminRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Session;
+use Illuminate\Support\Facades\Gate;
+
 class AdminController extends Controller
 {
-    public function store(StoreAdminRequest $request){
-        $ad = true;
-        User::create([
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password']),
-            'admin' => $ad,
-            'telefone' => $request['telefone'],
-            'cpf' => $request['cpf'],
-            'endereco' => $request['endereco'],
-        ]);
-        Session::flash('message', "Administrador cadastrado com sucesso!");
-        return redirect()->route('home');
+    public function store(StoreAdminRequest $request)
+    {
+        if (Gate::allows('admin')) {
+            $ad = true;
+            User::create([
+                'name' => $request['name'],
+                'email' => $request['email'],
+                'password' => Hash::make($request['password']),
+                'admin' => $ad,
+                'telefone' => $request['telefone'],
+                'cpf' => $request['cpf'],
+                'endereco' => $request['endereco'],
+            ]);
+            Session::flash('message', "Administrador cadastrado com sucesso!");
+            return redirect()->route('home');
+        } else {
+            return redirect()->route('home');
+        }
     }
-    public function register(){
-        return view('admin.criarAdmin');
+    public function register()
+    {
+        if (Gate::allows('admin')) {
+            return view('admin.criarAdmin');
+        } else {
+            return redirect()->route('home');
+        }
     }
-    
-
 }
